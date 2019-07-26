84f8d68a9f47f27bf3d1f74a265c81b632d80dfa
Student <example@example.com>
2019-07-26 13:50:50 -0700

    fix homepage, word display page

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require('sinatra')
 require('sinatra/reloader')
 require('pry')
+require('./lib/learning_dictionary')
 also_reload('lib/**/*.rb')
 
+Word.clear
+
+word1 = Word.new("learn", "gain or acquire knowledge of or skill in something")
+word1.save()
+word2 = Word.new("forget", "fail to remember")
+word2.save()
 
 get ('/') do
+  redirect to ('/welcome')
+end
+get ('/welcome') do
+  @words = Word.all
   erb(:welcome)
+end
+get ('/definitions/:word') do
+  @word = Word.find(params[:word].to_s.gsub(':',''))
+  @def = Word.find_def(params[:word].to_s.gsub(':',''))
+  erb(:word)
 end
--- lib/learning_dictionary.rb
+++ lib/learning_dictionary.rb
@@ -1,6 +1,52 @@
 require 'pry'
 
-Class Blank
-  def initialize
+class Word
+  @@words = []
+  attr_accessor(:word, :definition)
+  def initialize(word, definition=nil)
+    @word = word
+    @definition = definition
+  end
+  def save
+    @@words.push(self)
+  end
+  def self.all
+    @@words
+  end
+  def self.clear
+    @@words = []
+  end
+  def self.add(word, definition=nil)
+    @@words.push(Word.new(word, definition))
+  end
+  def ==(other_word)
+    (@word == other_word.word)
+  end
+  def self.find(word)
+    found_word = nil
+    @@words.each do |w|
+      if w.word == word
+        found_word = word
+        break
+      end
+    end
+    found_word
+  end
+  def self.find_def(word)
+    found_def = nil
+    @@words.each do |w|
+      if w.word == word
+        found_def = w.definition
+        break
+      end
+    end
+    found_def
+  end
+  def update(word, definition=nil)
+    @word = word
+    @definition = definition
+  end
+  def delete
+    @@words.delete(self)
   end
 end
